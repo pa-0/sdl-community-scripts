ec3babaa128b2d7149f01a1af2c92304c635ef1e
andreaGhisa <aghisa@sdl.com>
2020-03-04 14:43:57 +0200

    DSI Viewer: Migrate to studio 2020.

--- DSI Viewer/Sdl.Community.SdiViewer/ViewModel/DsiViewerViewModel.cs
+++ DSI Viewer/Sdl.Community.SdiViewer/ViewModel/DsiViewerViewModel.cs
@@ -13,7 +13,7 @@ namespace Sdl.Community.DsiViewer.ViewModel
 	{
 		private List<DsiModel> _documentStructureInformation;
 		private List<IComment> _comments;
-		private Document _activeDocument;
+		private IStudioDocument _activeDocument;
 		private readonly EditorController _editorController;
 		private readonly SegmentVisitor _segmentVisitor;
 
@@ -163,7 +163,7 @@ namespace Sdl.Community.DsiViewer.ViewModel
 			SetActiveDocument(e.Document);
 		}
 
-		private void SetActiveDocument(Document document)
+		private void SetActiveDocument(IStudioDocument document)
 		{
 			if (_activeDocument != null)
 			{

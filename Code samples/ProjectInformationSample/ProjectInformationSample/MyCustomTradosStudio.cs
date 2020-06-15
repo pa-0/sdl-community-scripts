910dfcef6e6d70e4af0c16b13faa95aa1aa8dc77
TinaCaputa <caputa.florentina.lucia@gmail.com>
2020-06-15 14:33:31 +0300

    Add code sample to show how to access the project status and project type properties using the public API

---
+++ Code samples/ProjectInformationSample/ProjectInformationSample/MyCustomTradosStudio.cs
@@ -0,0 +1,32 @@
+﻿using Sdl.Desktop.IntegrationApi;
+using Sdl.Desktop.IntegrationApi.Extensions;
+using Sdl.TranslationStudioAutomation.IntegrationApi;
+
+namespace ProjectInformationSample
+{
+	[ApplicationInitializer]
+    class MyCustomTradosStudio : IApplicationInitializer
+    {
+	    public void Execute()
+	    {
+		    var projectsController = GetProjectController();
+		    var allProjects = projectsController?.GetAllProjects();
+		    if (allProjects != null)
+		    {
+			    foreach (var proj in allProjects)
+			    {
+				    // Retrieve the needed information from the project
+				    var projectInfo = proj.GetProjectInfo();
+				    var status = projectInfo?.Status; // the current status of the project
+					var publicationStatus = projectInfo?.PublicationStatus; // the publication status of the project
+					var projectType = projectInfo?.ProjectType; // the type of the project
+			    }
+		    }
+	    }
+
+		private ProjectsController GetProjectController()
+		{
+			return SdlTradosStudio.Application.GetController<ProjectsController>();
+		}
+	}
+}

4b389c4a0008c69acf58f009f2b44ea9976aecf3
TinaCaputa <caputa_florentina_lucia@yahoo.com>
2020-07-07 11:52:16 +0300

    Update MyCustomTradosStudio.cs

--- Code samples/AddProjectOrigin_IconPath/MyCustomTradosStudio.cs
+++ Code samples/AddProjectOrigin_IconPath/MyCustomTradosStudio.cs
@@ -63,15 +63,14 @@ namespace AddProjectOrigin_IconPath
 		// Write the icon to the Plugins\Unpacked folder and use the icon's path from that folder in order to set the project's IconPath
 	    public string GetIconPath()
 		{ 
-		    var assemblyPath = Assembly.GetExecutingAssembly().Location;
-		    var assemblyName = Assembly.GetExecutingAssembly().GetName().Name;
-		    var targetPath = assemblyPath.Remove(assemblyPath.Length - assemblyName.Length - 4);
-		    targetPath = targetPath + "icon.ico";
+		   var assemblyPath = Assembly.GetExecutingAssembly().Location;
+			var directoryInfo = Directory.GetParent(assemblyPath);
+			var targetPath = Path.Combine(directoryInfo.FullName, "icon.ico");
 
-		    using (var fs = new FileStream(targetPath, FileMode.Create))
-		    {
-			    PluginResources.icon.Save(fs);
-		    }
+			using (var fs = new FileStream(targetPath, FileMode.Create))
+			{
+				Resources.icon.Save(fs);
+			}
 			return targetPath;
 	    }
 	}

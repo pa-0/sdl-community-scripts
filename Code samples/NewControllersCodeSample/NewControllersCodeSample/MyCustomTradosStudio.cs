4b3cd951b1690d390872b41bcdd9b1cdb14d4b71
andreaGhisa <aghisa@sdl.com>
2020-08-06 14:53:09 +0300

    Studio new controllers code sample.

---
+++ Code samples/NewControllersCodeSample/NewControllersCodeSample/MyCustomTradosStudio.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Forms;
+using Sdl.Desktop.IntegrationApi;
+using Sdl.Desktop.IntegrationApi.Extensions;
+using Sdl.TranslationStudioAutomation.IntegrationApi;
+using Sdl.TranslationStudioAutomation.IntegrationApi.Presentation.DefaultLocations;
+
+namespace NewControllersCodeSample
+{
+	[RibbonGroup("Sample.WelcomeRibbonGroup", Name = "Sample_Action",
+		ContextByType = typeof(WelcomeViewController))]
+	[RibbonGroupLayout(LocationByType = typeof(TranslationStudioDefaultRibbonTabs.HomeRibbonTabLocation))]
+	public class SampleWelcomeViewRibbonGroup : AbstractRibbonGroup
+	{
+	}
+
+	[RibbonGroup("Sample.TranslationMemoriesRibbonGroup", Name = "Sample_Action",
+		ContextByType = typeof(ReportsViewController))]
+	[RibbonGroupLayout(LocationByType = typeof(TranslationStudioDefaultRibbonTabs.HomeRibbonTabLocation))]
+	public class SampleTranslationMemoriesViewRibbonGroup : AbstractRibbonGroup
+	{
+	}
+
+	[Action("Sdl.Community.SampleControllers", Name = "Sample action", Icon = "icon", Description = "Sample Action")]
+	[ActionLayout(typeof(SampleWelcomeViewRibbonGroup), 20, DisplayType.Large)]
+	[ActionLayout(typeof(SampleTranslationMemoriesViewRibbonGroup), 20, DisplayType.Large)]
+	class BringBackTheButtonViewPartAction : AbstractAction
+	{
+		protected override void Execute()
+		{
+		}
+	}
+}
